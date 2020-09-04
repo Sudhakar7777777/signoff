794b48379c4f84a83434b0f61d9f6c65794d94e6
Sudhakar Balakrishnan <>
2020-09-04 01:56:12 -0700

    update

---
+++ file-service/src/main/java/com/sbk/signoff/fileservice/storage/StorageException.java
@@ -0,0 +1,12 @@
+package com.sbk.signoff.fileservice.storage;
+
+public class StorageException extends RuntimeException {
+
+	public StorageException(String message) {
+		super(message);
+	}
+
+	public StorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
