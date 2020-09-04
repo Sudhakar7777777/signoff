9280cb643a0e41ec7e5946ce6e0d8f13e99fa330
Sudhakar Balakrishnan <>
2020-09-04 01:56:00 -0700

    setup the file-service

--- file-service/src/main/java/com/sbk/signoff/fileservice/FileServiceApplication.java
+++ file-service/src/main/java/com/sbk/signoff/fileservice/FileServiceApplication.java
@@ -1,13 +1,27 @@
 package com.sbk.signoff.fileservice;
 
+import com.sbk.signoff.fileservice.storage.StorageProperties;
+import com.sbk.signoff.fileservice.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class FileServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
