26fe5aedb619b69e21501f9b012c823502ab685e
Sudhakar Balakrishnan <>
2020-09-04 09:37:34 -0700

    refactor module names and add app-core module.

---
+++ app-core/src/main/java/com/sbk/signoff/coreapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sbk.signoff.coreapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CoreAppApplication.class);
+	}
+
+}
